7dff25ce5da3d804533883b0fc97c2e531947993
Irina <hallo.ween@mail.ru>
2021-10-19 13:49:09 +0300

    add increase and rise options

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,19 +1,46 @@
+import {Component} from 'react';
 import './employers-list-item.css';
 
-const EmployersListItem = ({name, salary, increase}) => {
-    // debugger;
+class EmployersListItem extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        increase: false,
+        rise: false,
+    }
+}
+
+onIncrease = () => {
+    this.setState(({increase}) => ({
+        increase: !increase
+    }));
+}
+
+onRise = () => {
+    this.setState(({rise}) => ({
+        rise: !rise
+    }));
+}
+
+render() {
+    const {name, salary} = this.props;
+    const {increase, rise} = this.state;
+
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
         classNames += " increase";
     }
+    if (rise) {
+        classNames += " like";
+    }
 
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm " onClick={this.onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -25,6 +52,8 @@ const EmployersListItem = ({name, salary, increase}) => {
             </div>
         </li>
     )
+}
+
 }
 
 export default EmployersListItem;
