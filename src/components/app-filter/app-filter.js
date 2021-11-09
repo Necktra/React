103d18227a8d653c5ef531242006026fca268229
Ira <hallo.ween@mail.ru>
2021-11-09 17:29:43 +0300

    filter update

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,17 +1,64 @@
+import { Component } from 'react';
 import './app-filter.css';
 
-const AppFilter = () => {
+// class AppFilter extends Component{
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             filter: "",
+//         }
+//     }
+
+// onFilter = (e) => {
+//     if(e.target.innerText === "Все сотрудники") {
+//         this.setState({filter: "All"})
+//     }     
+//     if(e.target.innerText === "На повышение") {
+//         this.setState({filter: ""})
+//     }
+//     if(e.target.innerText === "З/П > 1000$") {
+//         this.setState({filter: ""})
+//     }
+// //console.dir(e);
+// }
+
+//     render() {
+//         return(<div className="btn-group">
+//             <button className="btn btn-light" type="button" onClick={this.onFilter}>
+//                 Все сотрудники
+//             </button>
+//             <button className="btn btn-outline-light" type="button" onClick={this.onFilter}>
+//                 На повышение
+//             </button>
+//             <button className="btn btn-outline-light" type="button" onClick={this.onFilter}>
+//                 З/П > 1000$
+//             </button>
+//         </div>)
+//     }
+// }
+
+const AppFilter = (props) => {
+
+    const buttonsData = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'moreThen1000', label: 'З/П > 1000$'},
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button className={`btn ${clazz}`} type="button" key={name} onClick={()=>props.onFilterSelect(name)}>
+            {label}
+        </button> 
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button className="btn btn-light" type="button">
-                Все сотрудники
-            </button>
-            <button className="btn btn-outline-light" type="button">
-                На повышение
-            </button>
-            <button className="btn btn-outline-light" type="button">
-                З/П > 1000$
-            </button>
+            {buttons}
         </div>
     )
 }
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -19,6 +19,8 @@ class App extends Component {
                 { name: "Carl W.", salary: 15000, increase: false, rise: false, id: 3 },
             ],
             term: "",
+            filter: "all",
+
         }
         this.maxId = 4;
     }
@@ -69,18 +71,33 @@ class App extends Component {
         this.setState({term})
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise)
+                case 'moreThen1000':
+                    return items.filter(item => item.salary > 1000)
+                    default:
+                        return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.searchEmp(data, term);
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter />
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 <EmployersList data={visibleData} onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
